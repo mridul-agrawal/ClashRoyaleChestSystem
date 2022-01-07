ef045027c42ec79eaffb7feac975db3ff547e818
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2022-01-07 14:07:37 +0530

    Reorganised project structure. Impmlemented UIHandler. Defined some functions to update Resource UI.

--- Assets/Scripts/Resources/ResourceHandler.cs
+++ Assets/Scripts/Resources/ResourceHandler.cs
@@ -22,21 +22,23 @@ public class ResourceHandler : SingletonGeneric<ResourceHandler>
     public void IncreaseGems(int valueToIncrease)
     {
         gems += valueToIncrease;
+        UIHandler.Instance.UpdateGemsUI(gems);
     }
 
     public void DecreaseGems(int valueToDecrease)
     {
         gems -= valueToDecrease;
+        UIHandler.Instance.UpdateGemsUI(gems);
     }
     public void IncreaseCoins(int valueToIncrease)
     {
         coins += valueToIncrease;
+        UIHandler.Instance.UpdateCoinsUI(coins);
     }
     public void DecreaseCoins(int valueToDecrease)
     {
         coins -= valueToDecrease;
+        UIHandler.Instance.UpdateCoinsUI(coins);
     }
-
-
 
 }
---
+++ Assets/Scripts/UI/UIHandler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandler : SingletonGeneric<UIHandler>
+{
+    [SerializeField]
+    private Text valueOfGems;
+    [SerializeField]
+    private Text valueOfCoins;
+
+    public void UpdateGemsUI(int gems)
+    {
+        valueOfGems.text = gems.ToString();
+    }
+
+    public void UpdateCoinsUI(int coins)
+    {
+        valueOfCoins.text = coins.ToString();
+    }
+
+}
