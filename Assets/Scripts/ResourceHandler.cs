6789f8d0054b37c90f6b35c38e202cd879da4724
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2022-01-07 13:49:27 +0530

    Defined some functions to handle Resources.

--- Assets/Scripts/ResourceHandler.cs
+++ Assets/Scripts/ResourceHandler.cs
@@ -8,4 +8,35 @@ public class ResourceHandler : SingletonGeneric<ResourceHandler>
     private int coins;
     [SerializeField]
     private int gems;
+
+    public int GetCoins()
+    {
+        return coins;
+    }
+
+    public int GetGems()
+    {
+        return gems;
+    }
+
+    public void IncreaseGems(int valueToIncrease)
+    {
+        gems += valueToIncrease;
+    }
+
+    public void DecreaseGems(int valueToDecrease)
+    {
+        gems -= valueToDecrease;
+    }
+    public void IncreaseCoins(int valueToIncrease)
+    {
+        coins += valueToIncrease;
+    }
+    public void DecreaseCoins(int valueToDecrease)
+    {
+        coins -= valueToDecrease;
+    }
+
+
+
 }
