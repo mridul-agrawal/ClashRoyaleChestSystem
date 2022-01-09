8f9bcb0f0d3a4ab4a9021d19a25a44001a339713
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2022-01-09 13:05:44 +0530

    Created Scriptable Object for Setting Chest Configuration Map.

--- Assets/Scripts/ScriptableObjects/ChestConfigs.cs
+++ Assets/Scripts/ScriptableObjects/ChestConfigs.cs
@@ -6,6 +6,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ChestConfigSO", menuName = "ScriptableObjects/Chests/ChestConfigurations")]
 public class ChestConfigs : ScriptableObject
 {
-    // [Serializable]
-    public Dictionary<ChestType, ChestScriptableObject> ChestConfiguration;
+    [Serializable]
+    public class ChestConfigPair
+    {
+        public ChestType ChestType;
+        public ChestScriptableObject ChestSO;
+    }
+
+    public ChestConfigPair[] ChestConfigurationMap;
 }
